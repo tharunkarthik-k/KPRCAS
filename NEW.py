c0a7870f4f88f5de9b15d291f31d0d73996f182e
Tharukarthik <tharunkarthikthilag@gmail.com>
2025-06-09 15:43:12 +0530

    html and css added

---
+++ NEW.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+print(a+b)
+print(type(a))
+c="string"
+d=True
+pi=3.14
+print(type(c))
+print(type(d))
+print(type(pi))
---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+//odd or even
+var number = 10;
+if(number % 2 ==0){
+    console.log("even")
+}
